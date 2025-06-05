06866ced8c64cd83b7a5f8e4763a5ce55b01e7b4
ricgoe <bluemchen_fenchel_0d@icloud.com>
2025-06-05 14:54:48 +0200

    First implementation of a db and index maker with same index to reference
    Co-authored-by: jaboll <jaboll-ai@users.noreply.github.com>

--- database/embeddings.py
+++ database/embeddings.py
@@ -1,32 +1,32 @@
 import numpy as np
 from pathlib import Path
 import cv2 as cv
+from PIL import UnidentifiedImageError
 import torch
 from torchvision.models.feature_extraction import create_feature_extractor
 import os
+from autoenc import AutoEncoder
 
 
 class Embedder:
-    def __init__(self, model_path: str, img: str | Path):
-        self._device = "cuda"
+    def __init__(self, model_path: str):
+        self._device = "mps"
         self._model = torch.load(model_path, weights_only=False, map_location=torch.device(self._device))
         self._return_nodes: dict = {"encoder.4": "encoding_layer", "decoder.5": "decoding_layer"}
         self._feature_extract = create_feature_extractor(self._model, return_nodes=self._return_nodes)
-        
-    
        
-    def process(self) -> None | torch.Tensor:
-        img = cv.imread(str(self.img_to_compare),cv.IMREAD_GRAYSCALE)
+    def process(self, img) -> None | torch.Tensor:
+        img = cv.imread(str(img),cv.IMREAD_GRAYSCALE)
         if img is None:
-            return
+            raise UnidentifiedImageError
         img = np.float32(img)/255
-        img_resized = cv.resize(img, self._struct_ds_size, interpolation=cv.INTER_AREA)
+        img_resized = cv.resize(img, (32,32), interpolation=cv.INTER_AREA)
         img_resized = np.clip(img_resized, 0, 1)
-        return torch.from_numpy(np.reshape(img_resized, (1, self._struct_ds_size[0]*self._struct_ds_size[1])))
+        return torch.from_numpy(np.reshape(img_resized, (1, 32*32)))
     
-    def gen_embedding(self) -> torch.Tensor:
+    def gen_embedding(self, img) -> torch.Tensor:
         with torch.no_grad():  # No gradients needed for evaluation
-            processed_img = self.process().to(self._device)
+            processed_img = self.process(img).to(self._device)
             outputs: dict[str, torch.Tensor] = self._feature_extract(processed_img)
         return outputs["encoding_layer"].cpu()#.view(self._struct_ds_size[0], self._struct_ds_size[1]).cpu()
         
--- database/img_datab.py
+++ database/img_datab.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from sqlmodel import SQLModel, Field, create_engine, Session, select
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from embeddings import Embedder
-from faiss import IndexFlatIP, IndexFlatL2
+from faiss import IndexFlatIP, IndexFlatL2, IndexIDMap
+import faiss
+from autoenc import AutoEncoder
+from tqdm import tqdm
 
 
 
@@ -14,30 +17,56 @@ class ImgEntry(SQLModel, table = True):
     path: str
     width: int
     height: int
-    faiss_id: int
+    #faiss_id: int
 
 class ImgDBMaker:
     def __init__(self, db_path: str, img_drive_path: Path):
         self.engine = create_engine(f"sqlite:///{db_path}")
-        self.idx = IndexFlatIP(64)
+        self.idx = IndexIDMap(IndexFlatIP(64))
+        self.embedder = Embedder(model_path="/Users/richardgox/Documents/4. Semester/GuglLens/database/autoencoder_full_131_best.pth")
         SQLModel.metadata.create_all(self.engine)
 
-    def populate_db(self):
-        for file in DATA_LOCATION.rglob("*"):
-            with Image.open(file) as img:
-                img_entry = ImgEntry(path=str(file), width=img.width, height=img.height)
-                self.write_to_db(img_entry)
-            break
-
+    def populate_db(self, n=None):
+        i = 0
+        for file in tqdm(DATA_LOCATION.rglob("*"), total=540_000):
+            if n is not None and i > n: break
+            try:
+                try:
+                    if file.is_dir(): continue
+                    with Image.open(file) as img:
+                        img_entry = ImgEntry(path=str(file), width=img.width, height=img.height)
+                    self.write_to_db(img_entry)
+                    i += 1
+                except UnidentifiedImageError:
+                    print(f'{file} is not a valid image')
+            except KeyboardInterrupt:
+                break
+        faiss.write_index(self.idx, 'Index_db.faiss')
+                
+        
     def write_to_db(self, img: ImgEntry):
-        with Session(self.engine) as session:
-            session.add(img)
-            session.commit()
-            session.refresh(img)
-        embedder = Embedder(model_path="autoencoder/ae.pt", img=img.path)
-        embedding = embedder.gen_embedding()
-        self.idx.add_with_ids(1, embedding, img.id) 
+        try:
+            with Session(self.engine) as session:
+                session.add(img)
+                session.commit()
+                session.refresh(img)
+            embedding = self.embedder.gen_embedding(img.path)
+            self.idx.add_with_ids(embedding, img.id)
+        except Exception:
+            with Session(self.engine) as session:
+                session.delete(img)
+                session.commit()
+            
 
 if __name__ == "__main__":
     db = ImgDBMaker("test.db", DATA_LOCATION)
     db.populate_db()
+    # fi = faiss.read_index('Index_db.faiss')
+    # print(fi.is_trained)
+    # embeddings = faiss.vector_to_array(fi.id_map)  # This is a numpy array (float32)
+    # print(embeddings.shape)  # (num_vectors, vector_dim)
+    # for i in range(1, 3742):
+    #     if i != embeddings[i-1]:
+    #         print(i, embeddings[i-1])
+    #         break
+    # print(embeddings[:-1])
