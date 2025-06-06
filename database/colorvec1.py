94cdf0fc8d204d93a1b56b9b4ac8b1e66eab6ff0
jaboll-ai <spexe0011@gmail.com>
2025-06-06 12:33:17 +0200

    vectorized histogram calculation

--- database/colorvec1.py
+++ database/colorvec1.py
@@ -10,23 +10,31 @@ class ColorVecCalculator:
         self.epsilon = 10e-10
     
     def filter_l_s(self, arr: np.ndarray):
-        mask = (arr[:,:,1] > 20) & (arr[:,:,2] > 50)
+        mask = (arr[...,1] > 20) & (arr[...,2] > 50)
         return arr[mask]
         
     
     def gen_color_vec(self, img_path: str):
         img = cv.imread(img_path)
-        h1_quant, l1_quant, s1_quant = self.quantize_channels(img)
-        his1_h = cv.calcHist([h1_quant], [0], None, [self.h_bins], [0, self.h_bins])
-        his1_h /= his1_h.sum() + self.epsilon
-        his1_l = cv.calcHist([l1_quant], [0], None, [self.l_bins], [0, self.l_bins])
-        his1_l /= his1_l.sum() + self.epsilon
-        his1_s = cv.calcHist([s1_quant], [0], None, [self.s_bins], [0, self.s_bins])
-        his1_s /= his1_s.sum() + self.epsilon
+        img = cv.cvtColor(img, cv.COLOR_BGR2HLS)
+        hls = self.quantize_channels(img)
+        hist = [np.unique(hls[:, i], return_counts=True)[-1] for i in range(hls.shape[-1])]
         
-        vec = np.concatenate((his1_h, his1_l, his1_s)).reshape(1, -1)
+        vec = np.concatenate(hist).reshape(1, -1).astype(np.float32)
         return vec
         
     def quantize_channels(self, hls_arr: np.ndarray):
-        h, l, s = self.filter_l_s(hls_arr)[:,0], hls_arr[:,:,1], hls_arr[:,:,2]
-        return np.clip(h//(180//self.h_bins),a_min=0, a_max=self.h_bins-1), np.clip(l //(256//self.l_bins),a_min=0, a_max=self.l_bins-1), np.clip(s//(256//self.s_bins),a_min=0, a_max=self.s_bins)
+        hls = self.filter_l_s(hls_arr)
+        hls = hls/np.array([180, 256, 256])
+        hls[...,0] = np.floor(hls[..., 0] * self.h_bins)
+        hls[...,2] = np.floor(hls[..., 2] * self.s_bins)
+        hls[...,1] = np.floor(hls[..., 1] * self.l_bins)
+        
+        hls[...,0][hls[..., 0] == self.h_bins] = self.h_bins-1  #edge case hue=180
+        hls[...,1][hls[..., 1] == self.l_bins] = self.l_bins-1  #edge case l=256
+        hls[...,2][hls[..., 2] == self.s_bins] = self.s_bins-1  #edge case s=256
+        return hls
+    
+if __name__ == "__main__":
+    cvc = ColorVecCalculator()
+    cvc.gen_color_vec('images/HSD_duesseldorf_IMG_9904.jpg')
