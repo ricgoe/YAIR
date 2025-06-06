6a96a5d9036952b085c78d1b2970141bd8a539be
jaboll-ai <spexe0011@gmail.com>
2025-06-06 13:28:40 +0200

    rewritten hist calculation again with numpy

--- database/colorvec1.py
+++ database/colorvec1.py
@@ -3,10 +3,9 @@ import numpy as np
 
 class ColorVecCalculator:
     
-    def __init__(self):
-        self.h_bins = 18
-        self.l_bins = 4
-        self.s_bins = 4
+    def _init_(self):
+        self.hls_bins = [18, 4, 4]
+        self.hls_max = [180, 256, 256]
         self.epsilon = 10e-10
     
     def filter_l_s(self, arr: np.ndarray):
@@ -18,7 +17,12 @@ class ColorVecCalculator:
         img = cv.imread(img_path)
         img = cv.cvtColor(img, cv.COLOR_BGR2HLS)
         hls = self.quantize_channels(img)
-        hist = [np.unique(hls[:, i], return_counts=True)[-1] for i in range(hls.shape[-1])]
+        hist = []
+        for i in range(hls.shape[-1]):
+            idx, counts = np.unique(hls[:, i], return_counts=True)
+            bins = np.zeros(self.hls_bins[i])
+            bins[idx.astype(int)] = counts #are ints anyway
+            hist.append(bins)
         
         vec = np.concatenate(hist).reshape(1, -1).astype(np.float32)
         return vec
@@ -26,15 +30,11 @@ class ColorVecCalculator:
     def quantize_channels(self, hls_arr: np.ndarray):
         hls = self.filter_l_s(hls_arr)
         hls = hls/np.array([180, 256, 256])
-        hls[...,0] = np.floor(hls[..., 0] * self.h_bins)
-        hls[...,2] = np.floor(hls[..., 2] * self.s_bins)
-        hls[...,1] = np.floor(hls[..., 1] * self.l_bins)
-        
-        hls[...,0][hls[..., 0] == self.h_bins] = self.h_bins-1  #edge case hue=180
-        hls[...,1][hls[..., 1] == self.l_bins] = self.l_bins-1  #edge case l=256
-        hls[...,2][hls[..., 2] == self.s_bins] = self.s_bins-1  #edge case s=256
+        for i in range(hls.shape[-1]):
+            hls[...,i] = np.floor(hls[..., i] * self.hls_bins[i])
+            hls[...,i][hls[..., i] == self.hls_bins[i]] = self.hls_bins[i]-1  #edge case hue=180
         return hls
     
 if __name__ == "__main__":
     cvc = ColorVecCalculator()
-    cvc.gen_color_vec('images/HSD_duesseldorf_IMG_9904.jpg')
+    cvc.gen_color_vec('/Volumes/Big Data/data/image_data/DAISY24/HSD_duesseldorf_IMG_9907.jpg')
