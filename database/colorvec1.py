b97f1b5f24df267b6f16e9ae532707956e7dde50
jaboll-ai <spexe0011@gmail.com>
2025-06-07 14:00:27 +0200

    adhearing to new api, return vec, height, width instead of only vector

--- database/colorvec1.py
+++ database/colorvec1.py
@@ -1,12 +1,12 @@
 import cv2 as cv
 import numpy as np
+from ImgModel import ImgConvertFailure
 
 class ColorVecCalculator:
     
-    def _init_(self):
+    def __init__(self):
         self.hls_bins = [18, 4, 4]
         self.hls_max = [180, 256, 256]
-        self.epsilon = 10e-10
     
     def filter_l_s(self, arr: np.ndarray):
         mask = (arr[...,1] > 20) & (arr[...,2] > 50)
@@ -14,18 +14,21 @@ class ColorVecCalculator:
         
     
     def gen_color_vec(self, img_path: str):
-        img = cv.imread(img_path)
-        img = cv.cvtColor(img, cv.COLOR_BGR2HLS)
-        hls = self.quantize_channels(img)
-        hist = []
-        for i in range(hls.shape[-1]):
-            idx, counts = np.unique(hls[:, i], return_counts=True)
-            bins = np.zeros(self.hls_bins[i])
-            bins[idx.astype(int)] = counts #are ints anyway
-            hist.append(bins)
+        try:
+            img = cv.imread(img_path)
+            img = cv.cvtColor(img, cv.COLOR_BGR2HLS)
+            hls = self.quantize_channels(img)
+            hist = []
+            for i in range(hls.shape[-1]):
+                idx, counts = np.unique(hls[:, i], return_counts=True)
+                bins = np.zeros(self.hls_bins[i])
+                bins[idx.astype(int)] = counts #are ints anyway
+                hist.append(bins)
+        except Exception as e:
+            raise ImgConvertFailure(img_path, e)
         
         vec = np.concatenate(hist).reshape(1, -1).astype(np.float32)
-        return vec
+        return vec, img.shape[0], img.shape[1]
         
     def quantize_channels(self, hls_arr: np.ndarray):
         hls = self.filter_l_s(hls_arr)
@@ -37,4 +40,4 @@ class ColorVecCalculator:
     
 if __name__ == "__main__":
     cvc = ColorVecCalculator()
-    cvc.gen_color_vec('/Volumes/Big Data/data/image_data/DAISY24/HSD_duesseldorf_IMG_9907.jpg')
+    cvc.gen_color_vec(r'images\HSD_duesseldorf_IMG_9904_crop.jpg')
