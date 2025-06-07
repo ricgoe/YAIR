b92df0e9edf359215e6323f93f23887cff860baf
jaboll-ai <spexe0011@gmail.com>
2025-06-07 14:03:51 +0200

    rewritten complete logic to be multi threadable

--- database/img_datab.py
+++ database/img_datab.py
@@ -1,42 +1,45 @@
 from pathlib import Path
 from sqlmodel import SQLModel, create_engine, Session
 from ImgModel import ImgEntry
-from PIL import Image, UnidentifiedImageError
-from embeddings import Embedder
+# from embeddings import Embedder
+# from autoenc import AutoEncoder
 from colorvec1 import ColorVecCalculator
 from faiss import IndexFlatIP, IndexFlatL2, IndexIDMap
 import faiss
-from autoenc import AutoEncoder
 from tqdm import tqdm
 import numpy as np
-
-DATA_LOCATION = Path("/Volumes/Big Data/data/image_data")
+from queue import Queue
+from collections.abc import Callable
+import threading
+from sqlalchemy.exc import IntegrityError
 
 class ImgDBMaker:
-    def __init__(self, db_path: str, img_drive_path: Path):
+    def __init__(self, db_path: str, index_path: Path, img_drive_path: Path, threads = 4, cap = None):
         self.engine = create_engine(f"sqlite:///{db_path}")
         SQLModel.metadata.create_all(self.engine)
-        self.idx = IndexIDMap(IndexFlatIP(26))
-        self.embedder = Embedder(model_path="/Users/richardgox/Documents/4. Semester/GuglLens/database/autoencoder_full_131_best.pth")
+        self.idx = faiss.read_index(str(index_path)) if index_path.is_file() else IndexIDMap(IndexFlatIP(26))
+        # self.embedder = Embedder(model_path="/Users/richardgox/Documents/4. Semester/GuglLens/database/autoencoder_full_131_best.pth")
         self.colorvec = ColorVecCalculator()
+        self.files_to_process = Queue(maxsize=threads+1)
+        self.vectors_to_index = Queue()
+        self.images_done = 0
+        self.worker_done = 0
+        self.img_drive_path = img_drive_path
+        self.index_path = index_path
+        self.threads = threads
+        self.kill_switch = threading.Event()
+        self.cap = cap
 
     def populate_db(self, n=None):
-        i = 0
-        for file in tqdm(DATA_LOCATION.rglob("*"), total=540_000):
-            if n is not None and i > n: break
-            try:
-                try:
-                    if file.is_dir(): continue
-                    with Image.open(file) as img:
-                        img_entry = ImgEntry(path=str(file), width=img.width, height=img.height)
-                    self.write_to_db(img_entry)
-                    i += 1
-                except UnidentifiedImageError:
-                    print(f'{file} is not a valid image')
-            except KeyboardInterrupt:
-                break
-        faiss.write_index(self.idx, 'Index_db.faiss')
-                
+        threading.Thread(target=self.enqueuer, args=(self.cap,)).start() # file_queue
+        for _ in range(self.threads):
+            threading.Thread(target=self.worker, args=(self.files_to_process, self.process_file, self.on_worker_done)).start() # calculate vector and write to output queue
+        try:
+            self.worker(self.vectors_to_index, self.write_to_db) # currently running on main thread. call line below for diffrent thread (?necessary?)
+            #threading.Thread(target=self.worker, args=(self.vectors_to_index, self.write_to_db)).start()
+        except KeyboardInterrupt:
+            self.kill_switch.set()
+        faiss.write_index(self.idx, str(self.index_path))
         
     def write_to_db(self, result: tuple[Path, np.ndarray]):
         img_path, height, width, vec = result
@@ -57,9 +60,53 @@ class ImgDBMaker:
                     self.idx.remove_ids(np.array([img.id]))
             finally:
                 session.commit()
-                self.idx.remove_ids(np.array([img.id]))
-            
+        self.images_done += 1
+        if self.images_done % 500 == 0: faiss.write_index(self.idx, str(self.index_path))
+                
+    def process_file(self, img_path: Path):
+        vec, height, width = self.colorvec.gen_color_vec(img_path)
+        # TODO concatinate with embeddings and weight properly
+        self.vectors_to_index.put((img_path, height, width, vec))
+        
+    def on_worker_done(self):
+        self.worker_done += 1
+        if self.worker_done >= self.threads:
+            self.vectors_to_index.put(None)
+        
+    def enqueuer(self, n=None):
+        i = 0
+        for file in self.img_drive_path.rglob("*"):
+            if file.is_dir():
+                continue
+            self.files_to_process.put(file)
+            i += 1
+            if n is not None and i >= n:
+                break
+            if self.kill_switch.is_set():
+                print("Enqueuer killed")
+                return
+        for _ in range(self.threads):
+            self.files_to_process.put(None)  # Sentinels to stop workers
+        print("Enqueuer exited gracefully")
+    
+    def worker(self, queue: Queue, func: Callable, exit_func: Callable=None):
+        while True:
+            result = queue.get()
+            if result is None:
+                if exit_func is not None: exit_func()
+                break
+            if self.kill_switch.is_set():
+                print("Worker killed")
+                return
+            func(result)
+            queue.task_done()
+        print("Worker exited gracefully")
 
+ 
 if __name__ == "__main__":
-    db = ImgDBMaker("test.db", DATA_LOCATION)
-    db.populate_db()
+    import time
+    # db = ImgDBMaker(Path("test.db"), Path('Index_db.faiss'), Path("/Volumes/Big Data/data/image_data"))
+    db = ImgDBMaker(Path("test.db"), Path('Index_db.faiss'), Path("images"), threads=4, cap=100)
+    start = time.time()
+    db.populate_db(n=100)
+    print(time.time()-start)
